68b12c49dfba507766e2e0e3e810cbec1c9af215
Nabeel Mansoori <136516572+MuhammadNabeel28@users.noreply.github.com>
2026-01-16 01:35:22 +0500

    Source Commit

--- lib/ui/views/admin/client/client_form_view.dart
+++ lib/ui/views/admin/client/client_form_view.dart
@@ -19,115 +19,149 @@ class ClientFormView extends StackedView<ClientFormViewModel> {
               fontSize: 20,
             )),
         content: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(
+          child: Form(
+            key: viewModel.formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Account Id',
                     labelText: 'Account Id',
                     hintStyle: AppFonts.regular(
                       fontSize: 10,
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Account Name',
-                  hintStyle: AppFonts.regular(
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Contact Info',
-                  hintStyle: AppFonts.regular(
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'User Id',
-                  hintStyle: AppFonts.regular(
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-              TextField(
-                obscuringCharacter: '*',
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintStyle: AppFonts.regular(
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-              TextField(
-                controller: viewModel.dateController,
-                readOnly: true,
-                decoration: InputDecoration(
-                  labelText: 'Date Of Expiry',
-                  hintStyle: AppFonts.regular(fontSize: 10),
-                ),
-                onTap: () async {
-                  final DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (pickedDate != null) {
-                    viewModel.pickedDate = pickedDate;
-                    final formattedDate =
-                        DateFormat('dd-MMM-yyyy').format(pickedDate);
-                    viewModel.dateController.text = formattedDate;
-                  }
-                },
-              ),
-              SizedBox(height: 24),
-              const Text('Account Type:',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  // Master
-                  GestureDetector(
-                    onTap: () => viewModel.toggleIsMaster(true),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Radio<bool>(
-                          value: true,
-                          groupValue: viewModel.ismaster,
-                          onChanged: (val) => viewModel.toggleIsMaster(true),
-                          visualDensity: VisualDensity.compact,
-                        ),
-                        const Text('Master'),
-                      ],
                     ),
                   ),
-
-                  const SizedBox(width: 32),
-
-                  // Client
-                  GestureDetector(
-                    onTap: () => viewModel.toggleIsMaster(false),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Radio<bool>(
-                          value: false,
-                          groupValue: viewModel.ismaster,
-                          onChanged: (val) => viewModel.toggleIsMaster(false),
-                          visualDensity: VisualDensity.compact,
-                        ),
-                        const Text('Client'),
-                      ],
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Account Name',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
                     ),
                   ),
-                ],
-              )
-            ],
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Contact Info',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'User Id',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Device Limit',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    hintText: 'example@domain.com',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email required';
+                    }
+                    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+                    if (!emailRegex.hasMatch(value)) {
+                      return 'Enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintStyle: AppFonts.regular(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: viewModel.dateController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    labelText: 'Date Of Expiry',
+                    hintStyle: AppFonts.regular(fontSize: 10),
+                  ),
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (pickedDate != null) {
+                      viewModel.pickedDate = pickedDate;
+                      final formattedDate =
+                          DateFormat('dd-MMM-yyyy').format(pickedDate);
+                      viewModel.dateController.text = formattedDate;
+                    }
+                  },
+                ),
+                SizedBox(height: 24),
+                const Text('Account Type:',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // Master
+                    GestureDetector(
+                      onTap: () => viewModel.toggleIsMaster(true),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Radio<bool>(
+                            value: true,
+                            groupValue: viewModel.ismaster,
+                            onChanged: (val) => viewModel.toggleIsMaster(true),
+                            visualDensity: VisualDensity.compact,
+                          ),
+                          const Text('Master'),
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(width: 32),
+
+                    // Client
+                    GestureDetector(
+                      onTap: () => viewModel.toggleIsMaster(false),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Radio<bool>(
+                            value: false,
+                            groupValue: viewModel.ismaster,
+                            onChanged: (val) => viewModel.toggleIsMaster(false),
+                            visualDensity: VisualDensity.compact,
+                          ),
+                          const Text('Client'),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
         actions: [
--- lib/ui/views/admin/client/client_form_viewmodel.dart
+++ lib/ui/views/admin/client/client_form_viewmodel.dart
@@ -7,6 +7,8 @@ class ClientFormViewModel extends BaseViewModel {
   late final DateTime? pickedDate;
   final TextEditingController dateController = TextEditingController();
   bool get ismaster => _ismaster;
+final _formKey = GlobalKey<FormState>();
+  GlobalKey<FormState> get formKey => _formKey;
   void toggleIsMaster(bool value) {
     print('kaam ki value $value');
     _ismaster = value;
